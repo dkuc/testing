7f4980766da7db2bf1c32773ab9a0a9ccd39319b
Dan Kuc <dkuc@redhat.com>
2017-07-12 14:07:49 -0400

    test

--- MessageQueue.js
+++ MessageQueue.js
@@ -23,7 +23,7 @@ module.exports = function () {
 
 
     function connect() {
-        return amqp.connect('amqp://172.30.89.123')
+        return amqp.connect('amqp://rabbitmq')
             .then(conn => conn.createChannel()
                 .then(ch => ch.assertExchange(exchangeName, 'direct', {durable: true})
                     .then(() => {
