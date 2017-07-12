1f1fcbdf4c7d045303d240c42ef7bcf3555857a9
Dan Kuc <dkuc@redhat.com>
2017-07-12 14:12:21 -0400

    test

--- producer.js
+++ producer.js
@@ -2,5 +2,5 @@
 const mq = require('./MessageQueue')();
 
 mq.connect()
-    .then(() => mq.publish('big', {account_number: 540155}))
+    .then(() => mq.publish('EvalStarted', {account_number: 540155}))
     .then(() => mq.close());
